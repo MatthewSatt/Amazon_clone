2afdbd52065d397cc7c2c17ae7d8961057f688aa
Matthew <MatthewSatterwhiteMS@gmail.com>
2022-04-20 22:16:07 -0700

    dropdown menu

---
+++ react-app/src/components/Dropdown/index.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { FaAlignJustify, FaArrowAltCircleLeft } from "react-icons/fa";
+import { Modal } from "../../context/Modal";
+import {getProducts} from '../../store/products'
+import {Link} from 'react-router-dom'
+import "./Dropdown.css";
+
+function Dropdown({ showSideNav, setShowSideNav }) {
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state?.productReducer);
+  const types = useSelector((state) => state?.typeReducer);
+
+  const openDropdown = () => {
+    setShowSideNav(true);
+  };
+
+  const closeModel = () => {
+      setShowSideNav(false)
+  }
+
+  useEffect(() => {
+      dispatch(getProducts())
+  }, [])
+  return (
+    <div className="dropdownmenu">
+      <FaAlignJustify onClick={openDropdown} />
+      <div className="dropdownopen">
+      {showSideNav &&
+      <Modal onClose={() => setShowSideNav(false)}>
+        <FaArrowAltCircleLeft id='close' onClick={closeModel}/>
+          <h2>Shop by...</h2>
+            <div className="producttypelinks">
+            {types && types?.map(type => (
+                <div className="navproducts" key={type?.id}>
+                    <Link id='navlinks'to={`/${type?.name}`}>
+                    {type?.name}
+                    </Link>
+                </div>
+            ))}
+            </div>
+        </Modal>}
+        </div>
+    </div>
+  );
+}
+
+export default Dropdown;
--- react-app/src/components/Home/index.js
+++ react-app/src/components/Home/index.js
@@ -1,10 +1,27 @@
 import React from 'react'
-import {useSelector} from 'react-redux'
+import {useDispatch, useSelector} from 'react-redux'
+import {useState, useEffect} from 'react'
+import { getProducts } from '../../store/products';
+import { getTypesThunk } from '../../store/types';
+import Dropdown from '../Dropdown';
+import './Home.css'
 
 function Home() {
-    const products = useSelector(state => Object.values(state.productReducer))
+  const dispatch = useDispatch()
+  const user = useSelector(state => state.session.user)
+  const products = useSelector(state => state.productReducer);
+  const types = useSelector((state) => state.typeReducer);
+  const [showSideNav, setShowSideNav] = useState(false)
+
+  useEffect(() => {
+    dispatch(getProducts());
+    dispatch(getTypesThunk());
+  }, []);
+
   return (
-    <div>Home</div>
+    <div className='home'>
+      <Dropdown showSideNav={showSideNav} setShowSideNav={setShowSideNav}/>
+    </div>
   )
 }
 
