62b7eafad9d028f2409ca2d14ab8701d6d649325
Matthew <MatthewSatterwhiteMS@gmail.com>
2022-04-23 01:00:11 -0700

    links updated to product pages

--- react-app/src/components/ProductTypes/Books/index.js
+++ react-app/src/components/ProductTypes/Books/index.js
@@ -1,28 +1,30 @@
-import React, { useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { getProductTypesThunk } from '../../../store/products'
-import { useParams } from 'react-router-dom'
-import "./Books.css"
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getProductTypesThunk } from "../../../store/products";
+import { useParams } from "react-router-dom";
+import {Link} from 'react-router-dom'
+import "./Books.css";
 
 function Books() {
-  const dispatch = useDispatch()
-  const products = useSelector(state => state.productReducer)
-  const {typeId} = useParams()
-
-
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state.productReducer);
+  const { typeId } = useParams();
 
   useEffect(() => {
-    dispatch(getProductTypesThunk(typeId))
-  }, [])
+    dispatch(getProductTypesThunk(typeId));
+  }, []);
 
   return (
-    <div>Books
-      {products && products.map(product => (
-        <div key={product.id}>{product.name}</div>
-      ))}
-
+    <div>
+      Books
+      {products &&
+        products.map((product) => (
+            <Link to={`/product/${product.id}`}>
+            <h1>{product.name}</h1>
+            </Link>
+          ))}
     </div>
-  )
+  );
 }
 
-export default Books
+export default Books;
