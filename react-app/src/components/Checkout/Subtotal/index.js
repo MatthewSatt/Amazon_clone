c0fa3ac5222653fcc0d963b74da7486f081ca7e0
Matthew <MatthewSatterwhiteMS@gmail.com>
2022-04-27 21:13:08 -0700

    subtotal refactor

--- react-app/src/components/Checkout/Subtotal/index.js
+++ react-app/src/components/Checkout/Subtotal/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getCartThunk } from '../../../store/cart'
 import "./Subtotal.css"
@@ -7,30 +7,40 @@ import "./Subtotal.css"
 function Subtotal() {
   const dispatch = useDispatch()
   const user = useSelector(state => state.session.user)
-  const products = useSelector(state => state.cartReducer)
+  const cart = useSelector(state => state.cartReducer)
+  const products = useSelector(state => state.productReducer)
+  const [length, setLength] = useState(0)
+  const [price, setPrice] = useState(0)
 
 
 
-  const totalItems = (array) => {
-    return array.length
-  }
+useEffect(() => {
+  setLength(cart.length)
+}, [cart.length])
 
-  const priceChecker = (array) => {
-    let total = 0
-    for(let i = 0; i < array.length; i++) {
-      let price = array[i].price
-      total += price
+useEffect(() =>  {
+  let total = 0
+  cart.forEach((item) => {
+    for(let i = 0; i < products.length; i++) {
+      let product = products[i]
+      if(product.id === item.product_id) {
+        total += product.price
+      }
+      setPrice(total.toFixed(2))
     }
-    return "$" + total.toFixed(2)
-  }
+  }, [cart.length])
+
+
+  // setPrice()
+})
 
   return (
     <div className='subtotal'>
     <div className='cartitemcount'>
-      <h1>Total Items ({totalItems(products)}):</h1>
+      <h1>Total Items ({length}):</h1>
     </div>
     <div className='cartprice'>
-      <h2>Price: {priceChecker(products)}</h2>
+      <h2>Price: {`$${price}`}</h2>
     </div>
     <button className='cartbutton'>Purchase</button>
 
