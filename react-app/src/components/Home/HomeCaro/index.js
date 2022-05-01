c390b296956f96fa796f8d97be4bb282255b9f54
Matthew <MatthewSatterwhiteMS@gmail.com>
2022-05-01 00:41:25 -0700

    images for homes

---
+++ react-app/src/components/Home/HomeCaro/index.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+
+function HomeCaro() {
+  const user = useSelector(state => state.session.user)
+  const history = useHistory()
+let slideIndex = 0;
+
+useEffect(() => {
+    showSlides()
+},[])
+
+function showSlides() {
+  if(history.location.pathname === '/' && user) {
+
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides?.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    if(slides === 0) {
+      slides[slideIndex].style.display = "block";
+    } else {
+      slides[slideIndex-1].style.display = "block";
+    }
+    setTimeout(showSlides, 2000);
+  } else {
+    return
+  }
+}
+  return (
+    <div className="slideshow-container">
+      <div className="mySlides fade">
+        <img
+          className="homeimages"
+          src="https://m.media-amazon.com/images/I/71P47BioYqL._SX3000_.jpg"
+        />
+      </div>
+
+      <div className="mySlides fade">
+        <img
+          className="homeimages"
+          src="https://m.media-amazon.com/images/I/61z+zr8oqXL._SX3000_.jpg"
+        />
+      </div>
+</div>
+  );
+}
+
+export default HomeCaro;
---
+++ react-app/src/components/Home/HomeCaro2/index.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+
+function HomeCaro2() {
+  const user = useSelector(state => state.session.user)
+  const history = useHistory()
+let slideIndex1 = 0;
+
+useEffect(() => {
+    showSlides1()
+},[])
+
+function showSlides1() {
+  if(history.location.pathname === '/' && user.id) {
+
+    let i;
+    let slides = document.getElementsByClassName("mySlides1");
+    for (i = 0; i < slides?.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slideIndex1++;
+    if (slideIndex1 > slides.length) {slideIndex1 = 1}
+    if(slides === 0) {
+      slides[slideIndex1].style.display = "block";
+    } else {
+      slides[slideIndex1-1].style.display = "block";
+    }
+    setTimeout(showSlides1, 2400);
+  }
+}
+  return (
+    <div className="slideshow-container">
+      <div className="mySlides1 fade">
+        <img
+          className="homeimages"
+          src="https://m.media-amazon.com/images/I/611YB+hYM3L._SX3000_.jpg"
+        />
+      </div>
+
+      <div className="mySlides1 fade">
+        <img
+          className="homeimages"
+          src="https://m.media-amazon.com/images/I/61lZZvJoB7L._SX3000_.jpg"
+        />
+      </div>
+</div>
+  );
+}
+
+export default HomeCaro2;
