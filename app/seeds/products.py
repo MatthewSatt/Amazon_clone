c0be67c37db1eb83af6d29d51545cccd529fd166
Matthew <MatthewSatterwhiteMS@gmail.com>
2022-05-01 14:49:33 -0700

    seeders

--- app/seeds/products.py
+++ app/seeds/products.py
@@ -789,8 +789,7 @@ def seed_products():
 
     )
 
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3DhkzTf_OxvG6690L3HHBVfb5GBK96_4eHA&usqp=CAU"className="movieimage"></img>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwM8sRQ_CrNfrWnrT-Pl2uvkfJZa5eA9ypgg&usqp=CAU"className="movieimage"></img>
+      
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTand8zTxuYCbBygvgJdXcXjtbM_Xx7OgGD2A&usqp=CAU"className="movieimage"></img>
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThY0TTfIT4MbS2tcS95yoH4MeJTfbe-w45FA&usqp=CAU"className="movieimage"></img>
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoUg2t__lqkZBIjqycnh_G70HR-Qq2yRmHCQ&usqp=CAU"className="movieimage"></img>
@@ -808,7 +807,7 @@ def seed_products():
         name="Replace",
         description="",
         price=1.00,
-        image="https://www.iams.com/sites/g/files/fnmzdf386/files/migrate-product-files/images/ugrp5w7bxhsd9ltxey7d.png"
+        image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3DhkzTf_OxvG6690L3HHBVfb5GBK96_4eHA&usqp=CAU"
 
     )
     movies3 = Product(
