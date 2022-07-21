5d314977fe5b3c0e3a438bb23bea69de9ab8e2e3
Matthew <MatthewSatterwhiteMS@gmail.com>
2022-07-21 14:15:22 -0700

    alert

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -41,6 +41,11 @@ def login():
         # Add the user to the session, we are logged in!
         user = User.query.filter(User.email == form.data['email']).first()
         login_user(user)
+        text_check = user.to_dict()
+        demo_check = text_check["username"]
+        if demo_check == "Demo":
+            print("hi")
+
         return user.to_dict()
     return {'errors': validation_errors_to_error_messages(form.errors)}, 401
 
